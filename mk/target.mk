48798915b5b8b6b37de763f409ef1d22603b67a5
Dusan Stevanovic <dule@wire.com>
2022-11-28 15:20:49 +0100

    Bump prebuilt webrtc to build 62 to fix assertion on linux

--- mk/target.mk
+++ mk/target.mk
@@ -243,7 +243,7 @@ endif
 #--- Generic settings -------------------------------------------------------
 
 ifeq ($(WEBRTC_VER),)
-WEBRTC_VER := 20220331.58
+WEBRTC_VER := 20220331.62
 endif
 
 JAVAC := javac
