2013652423a08c537aaa3115ca741b2d1782f889
Dusan Stevanovic <dule@wire.com>
2024-03-26 18:34:36 +0100

    android: fix MediaConverter to not write codec config frame to muxer

--- android/lib/src/main/java/com/waz/audioeffect/MediaConverter.java
+++ android/lib/src/main/java/com/waz/audioeffect/MediaConverter.java
@@ -3,6 +3,7 @@ package com.waz.audioeffect;
 import android.content.Context;
 import android.media.AudioFormat;
 import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
 import android.media.MediaFormat;
 import android.media.MediaExtractor;
 import android.media.MediaMuxer;
@@ -25,6 +26,7 @@ class MediaConverter {
 	private String inPath;
 	private String outPath;
 	private int bitrate = 90000;
+	private int samplerate = 44100;
 
 	public MediaConverter(String inPath, String outPath) {
 		this.inPath = inPath;
@@ -71,10 +73,10 @@ class MediaConverter {
 
 			try {
 				bitrate = mediaFormat.getInteger(MediaFormat.KEY_BIT_RATE);
-				Log.d(TAG, "decode: bitrate=" + bitrate);
+				samplerate = mediaFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE);
 			}
 			catch(Exception e) {
-				Log.d(TAG, "decode: bitrate key exception: " + e);
+				Log.w(TAG, "decode: bitrate key exception: " + e);
 			}
 			
 			extractor.selectTrack(i);		
@@ -169,33 +171,46 @@ class MediaConverter {
 			int trackId = muxer.addTrack(mediaFormat);			
 			boolean finished = false;
 			boolean eos = false;
-			String mimeType = mediaFormat.getString(MediaFormat.KEY_MIME);
+			String mimeType = mediaFormat.getString(MediaFormat.KEY_MIME, "audio/mp4a-latm");
 
 			encoder = MediaCodec.createEncoderByType(mimeType);
-			mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, bitrate); 
-			encoder.configure(mediaFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
-			encoder.start();
+
+			MediaFormat format = new MediaFormat();
+			format.setString(MediaFormat.KEY_MIME, mimeType);
+			format.setInteger(MediaFormat.KEY_BIT_RATE, bitrate);
+			format.setInteger(MediaFormat.KEY_CHANNEL_COUNT, 1);
+			format.setInteger(MediaFormat.KEY_SAMPLE_RATE, samplerate);
+			format.setInteger(MediaFormat.KEY_AAC_PROFILE,
+					  mediaFormat.getInteger(MediaFormat.KEY_AAC_PROFILE,
+								 MediaCodecInfo.CodecProfileLevel.AACObjectLC));
+
+			encoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
 			muxer.start();
+			encoder.start();
+			long ts = 0;
 			while(!finished) {
 				// Process input buffer
 				if (!eos) {
 					int idx = encoder.dequeueInputBuffer(QUEUE_TIMEOUT);
 					if (idx >= 0) {
-						ByteBuffer inputBuffer = encoder.getInputBuffer(idx);
-						if (inputBuffer == null) {
-							continue;
-						}
-
-						long sampleTime = 0;
-						int result;
-						
-						result = fc.read(inputBuffer);
-						if (result < 0) {
-							encoder.queueInputBuffer(idx, 0, 0, -1, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
-							eos = true;
+						ByteBuffer inBuf = encoder.getInputBuffer(idx);
+						if (inBuf == null) {
+							Log.d(TAG, "encode: no inbuf");
 						}
 						else {
-							encoder.queueInputBuffer(idx, 0, result, 0, 0);
+							long sampleTime = 0;
+							int result;
+
+							result = fc.read(inBuf);
+							if (result < 0) {
+								encoder.queueInputBuffer(idx, 0, 0, ts, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
+								eos = true;
+							}
+							else {
+								inBuf.rewind();
+								encoder.queueInputBuffer(idx, 0, result, ts, 0);
+								ts += (((result/2) * 1000000L) / samplerate);
+							}
 						}
 					}
 				}
@@ -207,10 +222,18 @@ class MediaConverter {
 					if (outBuf != null) {
 						outBuf.rewind();
 
-						muxer.writeSampleData(trackId, outBuf, bufInfo);
-						encoder.releaseOutputBuffer(idx, false);
-
 						finished = (bufInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0;
+						if (finished) {
+							bufInfo.presentationTimeUs = ts;
+						}
+
+						if ((bufInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
+							Log.d(TAG, "encode<mux>: skipping codec config");
+						}
+						else {
+							muxer.writeSampleData(trackId, outBuf, bufInfo);
+						}
+						encoder.releaseOutputBuffer(idx, false);
 					}
 				}
 			}
